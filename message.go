9f2b261b1da5f0e1dd707dfa330f3527c59c6013
Rajan Christian <15846532+thecybermafia@users.noreply.github.com>
2021-08-12 18:13:52 +0530

    Update message.go

--- message.go
+++ message.go
@@ -89,7 +89,7 @@ type Encoding string
 const (
 	// QuotedPrintable represents the quoted-printable encoding as defined in
 	// RFC 2045.
-	QuotedPrintable Encoding = "quoted-printable"
+	QuotedPrintable Encoding = "base64"
 	// Base64 represents the base64 encoding as defined in RFC 2045.
 	Base64 Encoding = "base64"
 	// Unencoded can be used to avoid encoding the body of an email. The headers
